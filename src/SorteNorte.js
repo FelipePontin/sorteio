e4975c70b2d40e0129ee307c1832cd021cf1933c
FelipePontin <felipe.pontin@norwaybank.com.br>
2023-03-20 11:01:51 -0300

    sorteio

--- src/SorteNorte.js
+++ src/SorteNorte.js
@@ -2,13 +2,19 @@ import React, { useState, useEffect } from 'react';
 import ImagemPresente from '../src/images/presente.svg'
 import ImagemPresenteAberto from '../src/images/presente_aberto.svg'
 import ImagemSorteioNumero from '../src/images/sorteio_numero.svg'
+import ImagemSorteioNome from '../src/images/sorteio_nome.svg'
+import ImagemSorteioRegistrado from '../src/images/sorteio_registrado.svg'
+import Logo from '../src/images/logo.svg'
 import './SorteNorte.css';
 
 const SorteNorte = () => {
   return (
-    <>
+    <section className='sorte_norte'>
       <header className='cabecalho'>
-        <div className='logo'>SORTE NORTE</div>
+        <div className='divisao_logo'>
+          <img className='imagem_logo' src={Logo}></img>
+          <div className='texto_logo'>SORTE NORTE</div>
+        </div>
         <div className='divisao_cabecalho'>
           <button className='botao_cabecalho botao_logar'>Logar</button>
           <button className='botao_cabecalho botão_registrar'>Registrar</button>
@@ -25,14 +31,41 @@ const SorteNorte = () => {
         <img className='imagem_introducao' src={ImagemPresenteAberto}></img>
       </section>
       <section className='sorteio_numero'>
-        <div>
-
+        <div className='divisao_sorteio_numero'>
+          <h1 className='titulo_sorteio_numero'>Sorteio de números</h1>
+          <p className='subtitulo_sorteio_numero'>Sorteio de números que permite que o organizador escolha uma quantidade entre um intervalo de números para sortear!</p>
         </div>
-        <div>
-          <img src={ImagemSorteioNumero}></img>
+        <div className='divisao_sorteio_numero_imagem'>
+          <img className='imagem_sorteio_numero' src={ImagemSorteioNumero}></img>
         </div>
       </section>
-    </>
+      <section className='sorteio_nome'>
+        <div className='divisao_sorteio_nome_imagem'>
+          <img className='imagem_sorteio_nome' src={ImagemSorteioNome}></img>
+        </div>
+        <div className='divisao_sorteio_nome'>
+          <h1 className='titulo_sorteio_nome'>Sorteio de nomes</h1>
+          <p className='subtitulo_sorteio_nome'>Preencha uma lista de nomes e escolha a quantidade de nomes que deverão ser sorteados desta lista!</p>
+        </div>
+      </section>
+      <section className='sorteio_registrado'>
+        <div className='divisao_sorteio_registrado'>
+          <h1 className='titulo_sorteio_registrado'>Sorteios registrados</h1>
+          <p className='subtitulo_sorteio_registrado'>Todos os sorteios que você faz são armazenados no nosso banco de dados, assim você não perde nada!</p>
+        </div>
+        <div className='divisao_sorteio_registrado_imagem'>
+          <img className='imagem_sorteio_registrado' src={ImagemSorteioRegistrado}></img>
+        </div>
+      </section>
+      <section className='bora_comecar'>
+        <h1 className='titulo_bora_comecar'>BORA COMEÇAR?</h1>
+        <button className='botao_bora_comecar'>REGISTRAR</button>
+      </section>
+      <footer className='rodape'>
+        <div className='texto_logo_rodape'>SORTE NORTE</div>
+        <p className='copy_rodape'>Copyright © 2023</p>
+      </footer>
+    </section>
   )
 }
 
